462eb7a68e0caa0bc0c7b65ca8bcc2cc671eaf5f
arpita-singh24 <85455106+arpita-singh24@users.noreply.github.com>
2024-03-07 20:10:51 +0530

    second commit

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,39 +1,3 @@
-// *****************correct code: playing when click on card**************************//
-
-// import React, { useRef, useState } from 'react';
-
-// const Card = ({ name, audioFile }) => {
-//     const audioRef = useRef(null);
-//     const [isPlaying, setIsPlaying] = useState(false);
-
-//     const handleCardClick = () => {
-//         if (isPlaying) {
-//             audioRef.current.pause();
-//         } else {
-//             audioRef.current.play();
-//         }
-//         setIsPlaying(!isPlaying);
-//     };
-
-//     return (
-//         <div className='card' onClick={handleCardClick}>
-//             <img src="https://img.freepik.com/free-vector/musical-notes-frame-with-text-space_1017-32857.jpg" alt="..." />
-//             <audio ref={audioRef} style={{ display: 'none' }}>
-//                 <source src={audioFile} type="audio/mpeg" />
-//                 Your browser does not support the audio element.
-//             </audio>
-//             <p>{name}</p>
-//         </div>
-//     );
-// };
-
-// export default Card;
-
-
-
-
-
-
 import React from 'react';
 
 const Card = ({ image, name, audioFile, onClick }) => {
